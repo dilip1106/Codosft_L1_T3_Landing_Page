387291fd1223491ead6e45933bd1ecfddd356959
dilip1106 <dilipteli38@gmail.com>
2024-07-07 01:37:44 +0530

    Completed the page

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const header= document.querySelector("header");
+const up= document.querySelector(".top");
+
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY > 150);
+    up.classList.toggle("up", window.scrollY > 150);
+})
+
+let menu =document.querySelector('#menu-icon');
+let navlist =document.querySelector('.navlist');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+window.onscroll=()=>{
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
